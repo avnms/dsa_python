c39e2c18941685a0c443b61b1497ea31d5568015
avnms <sarmaakondivnm@icloud.com>
2024-03-14 12:25:21 +1100

    Added function doc string for binary_search_steps_counter

--- binary_search/exercise.py
+++ binary_search/exercise.py
@@ -1,4 +1,23 @@
 def binary_search_steps_counter(input_list, item):
+    """
+    Return the number of steps it takes to search the target item in the given input list
+
+    Args:
+        input_list (list): A sorted list
+        item (integer): An integer
+
+    Returns:
+        steps_counter (integer): Maximum number of search steps
+
+    Examples:
+        >>> test_list_1 = [i + 1 for i in range(128)]
+        >>> binary_search_steps_counter(test_list_1, 128)
+        Returns 'The maximum number of steps to search for an item in a list of 128 values is: 8'
+
+        >>> test_list_2 = [i + 1 for i in range(256)]
+        >>> binary_search_steps_counter(test_list_2, 256)
+        Returns 'The maximum number of steps to search for an item in a list of 256 values is: 9'
+    """
     steps_counter = 0
     low = 0
     high = len(input_list) - 1
